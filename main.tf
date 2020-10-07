6d49dcfa6b09e00dc9733633735587d4abaeefee
Mateus Rodrigues <devrdgs@gmail.com>
2020-10-07 17:51:15 -0300

    added random_string resource

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region  = var.region
 }
 
+resource "random_string" "lb_id" {
+  length  = 4
+  special = false
+}
+
 # resource "aws_instance" "example" {
 #   ami           = "ami-0a52e8a6018e92bb0"
 #   instance_type = "t2.micro"
